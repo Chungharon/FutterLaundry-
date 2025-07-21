9ab049499cb4bc5d410c431c3294e0dbc6e817ca
Chungharon <ngairaharon@gmail.com>
2025-07-21 15:29:20 +0300

    29: Backend - firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:laundryhub/app.dart';
+import 'package:laundryhub/firebase_options.dart';
 
-
-void main() {
-
+Future<void> main() async {
   // Todo: Add Widgets Binding
-  // Todo: Init Locals Storage
+  // Todo: Init Local Storage
+  // Todo: Init Payment Methods
   // Todo: Await Native Splash
-  // Todo: Initialize Firebase
-  // Todo: Initialize Authentication
-  
+  // Todo: Initialze Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform).then(
+      (FirebaseApp value) => Get.put(AuthenticationRespository()
+    ),
+  );
+  // Todo: Initalize Authentication
+
+  // Load all the Material Design / Themes / Localizations / Bindings
   runApp(const App());
 }
-
-
-   
-
